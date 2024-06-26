7dd8b67fa1b0e8f7678b04a0b6fd6916fcd343fd
Maria Ragone <Maria.Ragone@samaritanministries.org>
2024-06-26 15:43:52 -0400

    app is set up with routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Missing from './Missing';
+import Week1 from './Week1';
+import Home from './Home';
+import Nav from './Nav';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Nav />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/week1' element={<Week1 />} />
+          <Route path='*' element={<Missing />} />
+        </Routes>
+
+
+      </Router>
+      <Footer />
+
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/Week1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Week1 = () => {
+  return (
+    <main>
+    Week1
+    
+    </main>
+  )
+}
+
+export default Week1
