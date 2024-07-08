5bf0b02161140862416bac6f517fd078d220fc80
Maria Ragone <maria@ragone.net>
2024-07-08 19:04:22 -0400

    added week 4 assignments

--- src/Week4.js
+++ src/Week4.js
@@ -3,8 +3,64 @@ import React from 'react'
 const Week4 = () => {
   return (
     <main className='Weeks'>
-    Week 2
-    
+
+<h1>Week 4</h1><br></br>
+<h1>Assignment 1</h1><br></br>
+
+Hi Corey,<br></br><br></br>
+
+Your essay provides clear and concise instructions on how to checker a wooden stock utilizing a decal pattern.<br></br><br></br>
+
+You have also highlighted the importance of checker patterns on a firearm for improving its aesthetics and for enhancing the shooter's grip and control.<br></br><br></br>
+
+Additionally, you have pointed out the distinction between different types of checker patterns, particularly the English style in the photo. The English style can also be distinguishable by its flat surface as opposed to the angled cuts of American checkering which produces pointed peaks. <br></br><br></br>
+
+It is hard to tell from the small photo but it seems like it could be American or English.<br></br><br></br>
+
+What type of checkering, if any, would you add to your stock?<br></br><br></br>
+
+Overall, your essay provides valuable information for anyone looking to checker their wooden stock using a decal pattern.<br></br><br></br>
+
+
+Thanks,<br></br>
+Maria<br></br><br></br>
+
+
+Hi Korey,<br></br><br></br>
+
+You forgot to include instructions on how to checker a wooden stock utilizing a decal pattern so this is the reason for the reduction in points.<br></br><br></br>
+
+There is also a late penalty of 10% per day in accordance with the SDI late policy.<br></br><br></br>
+
+Great job highlighting the importance of checker patterns on a firearm for improving its aesthetics and for enhancing the shooter's grip and control.<br></br><br></br>
+
+It is hard to tell from the small photo but it seems like it could be American or English.<br></br><br></br>
+
+Additionally, you have pointed out the distinction between different types of checker patterns, particularly the style in the photo. It is hard to tell from the small photo but it seems like it could be American or English.<br></br><br></br>
+
+What type of checkering, if any, would you add to your stock?<br></br><br></br>
+
+
+Thanks,<br></br>
+Maria<br></br><br></br>
+
+
+<h1>Week 4 Lab</h1>
+
+Hi Aaron,<br></br><br></br>
+
+Great work inletting the butt plate, lock, and cleaning the brass. Overall your process looks accurate and the inletting looks great so far. Did you cycle the lock to make sure it is functioning correctly?<br></br><br></br>
+
+I don't see any photos of inletting the toe plate so this is the reason for the reduction in points. <br></br><br></br>
+
+Thanks for following all the prompt instructions as well as providing the integrity check. I can tell you spent a lot of time making sure that the inletting was completed with care. The brass parts look nicely polished. You'll be handling them a lot more so they'll need a final polish later.<br></br><br></br>
+
+Overall it looks very well done.<br></br><br></br>
+
+Due to the delays with shipment of the kit I am not deducting points for this this assignment being late. <br></br><br></br>
+
+Thanks,<br></br>
+Maria   <br></br><br></br> 
     </main>
   )
 }
