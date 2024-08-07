1f5c2abbd75b953169e76572f3554b9011ae36f4
Maria Ragone <maria@ragone.net>
2024-08-06 20:32:01 -0400

    added week 7 module

--- src/Week7.js
+++ src/Week7.js
@@ -3,7 +3,37 @@ import React from 'react'
 const Week7 = () => {
   return (
     <main className='Weeks'>
-    Week 2
+      <h1>Week 7</h1><br></br>
+      <h1>Assignment 1</h1><br></br>
+
+      Week 7 Lab<br></br><br></br>
+
+
+Hi Matthew,<br></br><br></br>
+
+
+The steps you took this week are impressive. The stock looks exceptionally smooth. But you did not show any evidence of the screws being countersunk which was a part of this week's assignment and the reason for the reduction in points.<br></br><br></br>
+
+The steps you took this week are impressive. The stock looks exceptionally smooth. It appears your screws on the nose cap sit flush like they should.<br></br><br></br>
+
+How did your stock sanding go? I suggest that you try wetting the entire stock with a spray bottle or damp rag to raise the grain before completing the final sanding. This method will help you achieve an even smoother finish.<br></br><br></br>
+
+Did you need to remove any dents from the stock? It is amazing how easily dents can be raised with some steam.<br></br><br></br>
+
+Please don't hesitate to contact me if you have any further inquiries.<br></br><br></br>
+
+Thanks,<br></br>
+Maria<br></br><br></br>
+
+The steps you took this week are impressive. The stock looks exceptionally smooth, and the screws seem to be countersunk perfectly flush with the metal on the nose cap.<br></br><br></br>
+
+ It appears that you should work to countersink your screws a bit more on the nosecap.<br></br><br></br>
+
+
+There is a 10% late penalty for each day late per the SDI late policy.
+<br></br><br></br>
+
+It looks like you are making nice progress on your stock. But you did not show any evidence of the screws being countersunk which was a part of this week's assignment and the reason for the reduction in 8 points. There are also not enough photos of the stock for me to determine if the sanding and shaping was done. So this is the reason for a reduction in 15 points.<br></br><br></br>
     
     </main>
   )
